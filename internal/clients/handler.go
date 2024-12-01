81402efc066cc9a4ee1d1c4d9d2e3488c3a69af1
0sokrat0 <kobelevdaniil3101@yandex.ru>
2024-12-01 16:05:26 +0300

    Adding clients handler CRUD

--- internal/clients/handler.go
+++ internal/clients/handler.go
@@ -2,6 +2,7 @@ package clients
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/0sokrat0/GoGRAFFApi.git/pkg/db"
 	"github.com/0sokrat0/GoGRAFFApi.git/pkg/db/models"
@@ -22,4 +23,72 @@ func CreateClientHandler(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, client)
+}
+
+func GetAllClientsHandler(c *gin.Context) {
+	var clients []models.Clients
+
+	if err := db.DB.Find(&clients).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to get clients"})
+		return
+	}
+
+	c.JSON(http.StatusOK, clients)
+}
+
+func GetClientHandler(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "failed to get client"})
+		return
+	}
+	var client models.Clients
+	if err := db.DB.First(&client, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "client not found"})
+		return
+	}
+	c.JSON(http.StatusOK, client)
+}
+
+func UpdateClientHandler(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid client id"})
+		return
+	}
+
+	var client models.Clients
+	if err := db.DB.First(&client, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "client not found"})
+		return
+	}
+
+	var input models.Clients
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "failed to update client"})
+		return
+	}
+	c.JSON(http.StatusOK, client)
+}
+
+func DeleteClientHandler(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid client id"})
+		return
+	}
+
+	if err := db.DB.Delete(&models.Clients{}, id).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to delete client"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "client deleted successfully"})
 }
